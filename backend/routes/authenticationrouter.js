4bdc7868a09378f9079d7894457f379259a76e9c
Anmol Kumar <1RN20IS020@RNSIT.onmicrosoft.com>
2024-03-13 18:45:06 +0530

    final commit

---
+++ backend/routes/authenticationrouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../controllers/authentication");
+
+router.post("/register", auth.registration);
+router.post("/login", auth.login);
+
+module.exports = router;
